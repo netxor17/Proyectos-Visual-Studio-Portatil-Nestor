23f3cdb62848789221d8ae67cef85d3d2b8f0d15
netxor17 <1knestor.marin@gmail.com>
2023-09-29 11:21:28 +0200

    ejercixio 11 fal

---
+++ EliminarCasosErroneosFAL/Source.cpp
@@ -0,0 +1,74 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+// función que resuelve el problema
+vector <long long int> resolver(const vector<long long int>  &datos, long long int erroneo) {
+    vector<long long int> sol;
+    int i = 0;
+    while (i < datos.size() && datos[i] != erroneo)
+    {
+        sol.push_back(datos[i]);
+        ++i;
+	    
+    }
+    return sol;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int numElem;
+	long long int valorErroneo;
+    cin >> numElem >> valorErroneo;
+
+    vector<long long int> v(numElem);
+
+    for(int i =0; i < numElem; ++i)
+    {
+        cin >> v[i];
+    }
+
+    vector<long long int> sol = resolver(v, valorErroneo);
+    // escribir sol
+    if (sol.size() == 0) cout << "0" << endl;
+    for(int i = 0; i < sol.size(); ++i)
+    {
+        cout << sol[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
