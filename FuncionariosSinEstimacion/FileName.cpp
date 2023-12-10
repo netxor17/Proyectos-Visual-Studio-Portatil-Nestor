05519a1e1cf3857103d9d0e98386de893cd401c6
netxor17 <1knestor.marin@gmail.com>
2023-12-10 17:53:34 +0100

    Ejercicio de vuelta atras con optimizacion

---
+++ FuncionariosSinEstimacion/FileName.cpp
@@ -0,0 +1,102 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+bool esValida(vector<int>& marcas, const int& k, const int& i) {
+    if (marcas[i] > 1) {//el trabajo se ha asignado
+        return false;
+    }
+    return true;
+}
+// Ramas: el coste de cada trabajo
+// Altura: cada funcionario
+// 
+// función que resuelve el problema
+void funcionariosVA(const vector<vector<int>>& tiempos, vector<int>& sol, int k, int& acum, int& total,vector<int> &marcas) {
+    for (int i = 0; i < tiempos.size(); ++i) {//recorro las ramas
+        sol[k] = i;
+        //marco
+        total += tiempos[k][i];
+        marcas[i]++;
+        //he marcado
+        if (esValida(marcas, k, i)) {
+            if (k == tiempos.size() - 1) { //solucion final
+                if (total < acum) acum = total;
+            }
+            else {
+                funcionariosVA(tiempos, sol, k + 1, acum, total, marcas);
+            }
+        }
+        //desmarco
+        total -= tiempos[k][i];
+        marcas[i]--;
+    }
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int nFuncionarios;
+    cin >> nFuncionarios;
+
+    if (nFuncionarios == 0)
+        return false;
+
+    vector<vector<int>>tiempos;
+    tiempos.resize(nFuncionarios, vector<int>(nFuncionarios));
+    //leo los datos
+    for (int i = 0; i < nFuncionarios; ++i) {
+        for (int j = 0; j < nFuncionarios; ++j) {
+            cin >> tiempos[i][j];
+        }
+    }
+
+    vector<int> sol(nFuncionarios);//vector solucion
+
+    int j = 0, acum = 0;
+    for (int i = 0; i < nFuncionarios; ++i) {
+        sol[i] = tiempos[i][j];
+        acum += sol[i];
+        ++j;
+    }
+
+    vector<int> marcas(nFuncionarios,0);
+
+    int total = 0;
+    funcionariosVA(tiempos, sol, 0, acum, total, marcas);
+    // escribir sol
+    cout << acum << "\n";
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
